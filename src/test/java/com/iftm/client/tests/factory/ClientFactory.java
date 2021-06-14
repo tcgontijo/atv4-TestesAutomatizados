50ab8b6b089500276a72b3958041737efcf0cc64
Tulio Gontijo <tulio_gontijo@hotmail.com>
2021-06-13 22:56:52 -0300

    Projeto criado

--- src/test/java/com/iftm/client/tests/factory/ClientFactory.java
+++ src/test/java/com/iftm/client/tests/factory/ClientFactory.java
@@ -10,9 +10,17 @@ public class ClientFactory {
 	public static Client createClient() {
 		return new Client(1L, "Pablo Alberto", "12345678900", 2000.00, Instant.parse("1958-09-20T08:00:00Z"), 1);
 	}
+	
+	public static Client createEmptyClient() {
+		return new Client();
+	}
 
 	public static ClientDTO createClientDTO() {
 		return new ClientDTO(createClient());
+	}
+	
+	public static ClientDTO createEmptyClientDTO() {
+		return new ClientDTO(createEmptyClient());
 	}
 
 }
